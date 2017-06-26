12625c0a38e0f8441bf7bb0cd53d2b183b3b0aab
Joe Wass <jwass@crossref.org>
2017-06-26 11:00:18 +0100

    Bump Agent Framework to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject event-data-wikipedia-agent "0.1.9"
+(defproject event-data-wikipedia-agent "0.2.0"
   :description "Event Data Wikipedia Agent"
   :url "http://eventdata.crossref.org"
   :license {:name "The MIT License (MIT)"
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.nrepl "0.2.12"]
                  [org.clojure/core.async "0.2.391"]
-                 [org.crossref.event-data-agent-framework "0.1.18"]
+                 [org.crossref.event-data-agent-framework "0.2.0"]
                  [event-data-common "0.1.29-SNAPSHOT"]
                  [robert/bruce "0.8.0"]
                  [throttler "1.0.0"]
@@ -16,6 +16,5 @@
   :java-source-paths ["src-java"]
   :target-path "target/%s"
   :jvm-opts ["-Duser.timezone=UTC"]
-  :profiles {:uberjar {:aot :all}
-             :prod {:resource-paths ["config/prod" "lib/socketio.jar"]}
-             :dev  {:resource-paths ["config/dev" "lib/socketio.jar"]}})
+  :profiles {:uberjar {:aot :all}})
+
